68af7ed37b7cdf01ccb62fc935d66c28cdcd23b8
Udemezue John <johnzeus14@gmail.com>
2021-04-04 18:40:04 +0100

    updated landing page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
 
 
-		<nav class=" px-12 lg:px-24 py-3 flex justify-between item-center">
+		<nav class=" px-12 lg:px-24 py-4 flex justify-between item-center">
 
 			<div class="flex item-center space-x-3">
 
@@ -12,16 +12,27 @@
 
 			</div>
 
+			<div class="flex justify-between space-x-6 item-center">
+
+				<a href="" class="text-gray-800 font-body font-medium text-md tracking-wider hover:text-gray-900">Courses</a>
+				<a href="" class="text-gray-800 font-body font-medium text-md tracking-wider hover:text-gray-900"> About </a>
+
+				<a href="" class="text-gray-800 font-body font-medium text-md tracking-wider hover:text-gray-900">Blog </a>
+				<a href="" class="text-gray-800 font-body font-medium text-md tracking-wider hover:text-gray-900">Contact </a>
+				
+
+			</div>
+
 			<div class="item-center block lg:hidden ">
 				<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"></path></svg>
 			</div>
 
-			<div class="flex item-center space-x-3 hidden lg:block">
+			<div class="flex justify-between item-center space-x-3 hidden lg:block">
 				
-				<a href="" class="text-gray-800 font-body font-bold text-md font-black tracking-wider hover:text-gray-900 "> Courses</a>
+				<a href="" class="text-gray-800 font-body font-medium text-md  tracking-wider ring-1 ring-green-600 px-3 py-2 rounded-lg hover:text-gray-900 "> Join</a>
 
 
-				<a href="" class="bg-green-600 text-white px-3 py-2 rounded-lg font-bold text-md font-black tracking-wider"> Apply</a>
+				<a href="" class="bg-green-600 text-white px-3 py-2 rounded-lg font-medium text-md tracking-wider"> Login</a>
 
 				<!-- The dropdown section -->
 
--- src/views/Landing/Landing.vue
+++ src/views/Landing/Landing.vue
@@ -49,8 +49,8 @@
 
 <!-- The about us section -->
 
-<section class="mt-12 space-y-2">
-	<div class="space-y-3">
+<section class="mt-12 space-y-4">
+	<div class="space-y-4">
 
 		<h1 class=" text-3xl tracking-wider font-body font-bold"> Courses.</h1>
 
@@ -59,18 +59,18 @@
 		
 	</div>
 
-	<div class="grid grid-cols-1 lg:grid-cols-4 gap-4">
+	<div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
 
-		<div class="bg-white px-4 py-6 space-y-3 shadow rounded-lg">
+		<div class="bg-white px-4 py-6 space-y-6 shadow rounded-lg">
 
-			<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-ad mx-auto" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <rect x="3" y="5" width="18" height="14" rx="2" />
-  <path d="M7 15v-4a2 2 0 0 1 4 0v4" />
-  <line x1="7" y1="13" x2="11" y2="13" />
-  <path d="M17 9v6h-1.5a1.5 1.5 0 1 1 1.5 -1.5" />
-			</svg>
+			<img src="/img/digital-marketing.svg" class="w-full h-96">
+<!-- 			
+		
+ -->
+			<h1 class="text-center text-gray-800 tracking-wider font-body text-lg"> 
 
-			<h1 class="text-center text-gray-800 tracking-wider font-body text-base"> Digital Marketing</h1>
+
+			Digital Marketing</h1>
 
 			<p class="text-center text-gray-800 tracking-wider font-body text-base">
 				
@@ -84,30 +84,22 @@
 		</div>
 		<div class="bg-white px-4 py-6 space-y-3 shadow rounded-lg">
 
-			<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-analytics mx-auto" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <rect x="3" y="4" width="18" height="12" rx="1" />
-  <line x1="7" y1="20" x2="17" y2="20" />
-  <line x1="9" y1="16" x2="9" y2="20" />
-  <line x1="15" y1="16" x2="15" y2="20" />
-  <path d="M8 12l3 -3l2 2l3 -3" />
-</svg>
+			<img src="/img/data-science.svg" class="w-full h-96">
 
-			<h1 class="text-center text-gray-800 tracking-wider font-body text-base"> Data Science</h1>
+
+			<h1 class="text-center text-gray-800 tracking-wider font-body text-lg"> Data Science</h1>
 
 	
 
 		</div>
 
 		<div class="bg-white px-4 py-6 space-y-3 shadow rounded-lg">
+			<img src="/img/product-design.svg" class="w-full h-96">
 
-			<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-producthunt mx-auto" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M10 16v-8h2.5a2.5 2.5 0 1 1 0 5h-2.5" />
-  <circle cx="12" cy="12" r="9" />
-</svg>
+	
 
-			<h1 class="text-center text-gray-800 tracking-wider font-body text-base"> Product Design</h1>
+			<h1 class="text-center text-gray-800 tracking-wider font-body text-lg"> 
+			Product Design</h1>
 
 	
 
@@ -142,11 +134,11 @@
 
 <!-- The email marketing section -->
 
-<section class="mt-32">
+<section class="mt-32 space-y-3">
 
 	<h1 class="text-center text-3xl font-body font-bold text-gray-900 tracking-wider mt-18"> Join Our Email Subscribers List</h1>
 		<!--  The email marketing form -->
-	<div class="mt-12 flex justify-center">
+	<div class=" flex justify-center">
 		
 
 	</div>
@@ -191,11 +183,16 @@ export default{
 
 	}
 },
+computed:{
 
+	
+},
 methods:{
 
 
 }
+
+
 
 
 }
